be2e25c90d8a7d9778bbc6347ab3e8ebcf506fd2
Cameron Chandler <webstormcam@gmail.com>
2023-05-28 19:48:23 -0400

    Was able to connect and return updated text

--- javascript/script.js
+++ javascript/script.js
@@ -1,17 +1,26 @@
-fetch('https://api.openai.com/v1/edits',{
-method:"POST",
-headers:{
-    'Authorization': '',
-    'Content-Type': 'application/json'
-},
-body: JSON.stringify({
-    model:"text-davinci-edit-001",
-    temperature:0.8,
-    input: "Hello my name is bob and I am a cool guy that likes to explore countires like Ecador and Mex and puru",
-    instruction: "Fix the spelling mistakes",
+const textArea = document.getElementById('userInput')
+const processButton = document.getElementById('processButton')
+
+processButton.addEventListener('click',function(){
+    fetch('https://api.openai.com/v1/edits',{
+        method:"POST",
+        headers:{
+            'Authorization': ''
+            ,
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            model:"text-davinci-edit-001",
+            temperature:0.5,
+            input: textArea.value,
+            instruction: "Fix the spelling mistakes",
+        })
+        })
+        .then(res=>res.json())
+        .then((data)=>{
+            console.log(data)
+            textArea.value = data.choices[0]['text']
+        })
 })
-})
-.then(res=>res.json())
-.then((data)=>{
-    console.log(data)
-})
+
+
